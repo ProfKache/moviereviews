253de731f1a8f847f2615fe548c0d279e2733dbb
Salim Kachemela <sakachemela@gmail.com>
2022-10-11 22:22:25 +0300

    Add a display page for the movie list

--- apps/movie/models.py
+++ apps/movie/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from apps.helpers.models import TrackingModel
 
-# Create your models here.
+
+class Movie(TrackingModel):
+    """A class representing a table for storing movie info"""
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='movies/images/')
+    year = models.PositiveIntegerField('Year Released', blank=True)
+    url = models.URLField(blank=True)
+
+    class Meta(TrackingModel.Meta):
+        """Extra info about the Movie model"""
--- apps/movie/views.py
+++ apps/movie/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Movie
 
-# Create your views here.
+
+def index(request):
+    """Returns the homepage for the site"""
+    search_term = request.GET.get('search_movie') or ''
+    if search_term:
+        movies = Movie.objects.filter(title__icontains=search_term)
+    else:
+        movies = Movie.objects.all()
+    ctx = {'search_term': search_term, 'movies': movies}
+    return render(request, 'movie/index.html', ctx)
+
+
+def about(request):
+    """Returns the about page for the site"""
+    return HttpResponse('<h1>About Us</h1>')
+
+
+def signup(request):
+    """Returns the about page for the site"""
+    email = request.GET.get('email') or ''
+    ctx = {'email': email}
+    return render(request, 'movie/signup.html', ctx)
